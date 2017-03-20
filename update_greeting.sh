07ffdb52b8cd958ce23c7dfbc36b5fe2a48ff085
arseny <fomars@gmail.com>
2017-03-20 22:14:42 +0300

    update greeting updater

--- update_greeting.sh
+++ update_greeting.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo $1
 curl -X POST -H "Content-Type: application/json" -d "{
-  \"setting_type\":\"greeting\",
-  \"greeting\":{
-    \"text\": "Play Tic-tac-toe with GameClub bot. See the menu to start new game"
-  }
-}" "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$PAGE_ACCESS_TOKEN"
+  \"greeting\":[{
+    \"text\": \"Play Tic-tac-toe with GameClub bot. See the menu to start new game\",
+    \"locale\":\"default\",
+  }]
+}" "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=$PAGE_ACCESS_TOKEN"
